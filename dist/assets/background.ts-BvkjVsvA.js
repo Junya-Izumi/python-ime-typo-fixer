a33d1f3a3717897750a28c46cbc076997f8bf218
George Neo <george.neokq@gmail.com>
2026-02-23 18:10:54 +0800

    chore: remove dist

--- dist/assets/background.ts-BvkjVsvA.js
+++
@@ -1,2 +0,0 @@
-chrome.runtime.onInstalled.addListener(e=>{if("install"==e.reason){const e={isActive:!0};chrome.storage.local.set({setting:e})}});
-//# sourceMappingURL=background.ts-BvkjVsvA.js.map
--- dist/assets/globals-q3lYkHuB.js
+++
@@ -1,2 +0,0 @@
-const e=!1,t=(e,t=document)=>t.querySelector(e),o=()=>new Promise(e=>chrome.storage.local.get(["setting"],t=>{e(t.setting)})),s=e=>{chrome.storage.local.set({setting:e})},a=e=>null!=e&&"object"==typeof e&&"isActive"in e&&"boolean"==typeof e.isActive;export{e as d,o as g,a as i,t as q,s as u};
-//# sourceMappingURL=globals-q3lYkHuB.js.map
